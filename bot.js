19c3dd4d8edc620b34d37d7d2bd6db3a8cfd3c8d
SJ Bots\結城あやの <YuukiAyano@SJBots.com>
2021-04-12 08:20:23 +0900

    Setup Database

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 
 const token = process.env.token;
 const prefix = process.env.prefix;
-// const prefix = process.env.prefix;
-// const prefix = process.env.prefix;
-// const prefix = process.env.prefix;
-// const prefix = process.env.prefix;
+// const address = process.env.address;
+// const account = process.env.account;
+// const password = process.env.password;
+// const database = process.env.database;
 
 chaos.on('ready', () => {
   console.log(`Logged in as ${chaos.user.tag}!`);
