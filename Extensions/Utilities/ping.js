21e5702cc3e3ce746da12d977bc033f6d372ee6f
SJ Bots\結城あやの <YuukiAyano@SJBots.com>
2021-04-13 15:36:02 +0900

    Fix some bugs

--- Extensions/Utilities/ping.js
+++ Extensions/Utilities/ping.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
-	async execute(msg, args, prefix, connection, command) {
-		const waiting = await msg.channel.send('正在檢測延遲');
-		const ping = new Discord.MessageEmbed()
-			.setColor('RANDOM')
-			.setTitle('Pong!')
-			.setDescription(`延遲${0 - (Date.now() - msg.createdTimestamp)}ms.`)
-			.setFooter('Copyright © 結城あやの From SJ Bots');
-		await msg.channel.send(ping);
-		await waiting.delete({ timeout: 1000 });
-	},
+  name: 'ping',
+  description: 'Ping!',
+  async execute(msg, args, prefix, connection, command) {
+    const waiting = await msg.channel.send('正在檢測延遲');
+    const ping = new Discord.MessageEmbed()
+      .setColor('RANDOM')
+      .setTitle('Pong!')
+      .setDescription(`延遲${Math.abs(Date.now() - msg.createdTimestamp)}ms.`)
+      .setFooter('Copyright © 結城あやの From SJ Bots');
+    await msg.channel.send(ping);
+    await waiting.delete({ timeout: 1000 });
+  },
 };
--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const chaos = new Discord.Client();
 const fs = require('fs');
 const mysql = require('mysql');
-const Levels = require('discord-xp');
-const Canvacord = require('canvacord');
-const image = getImageSomehow();
+// const Levels = require('discord-xp');
+// const Canvacord = require('canvacord');
+// const image = getImageSomehow();
 
 require('dotenv').config();
 
@@ -41,7 +41,7 @@ for (const folder of commandFolders) {
 chaos.on('ready', () => {
   chaos.user.setPresence({
     activity: {
-      name: 'Powered by 結城あやの | Using ?help',
+      name: `Powered by 結城あやの | Using ${prefix}help`,
       type: 'STREAMING',
     },
     status: 'dnd',
